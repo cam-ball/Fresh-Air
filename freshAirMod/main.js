6c64ed4a24bcba9d84d2b4857a08b31d614e05a1
Lauren Ball <lauren.l.dev@gmail.com>
2022-06-04 20:03:34 -0400

    Add separate grandmapocalypse background

--- freshAirMod/main.js
+++ freshAirMod/main.js
@@ -1,7 +1,18 @@
 Game.registerMod("fresh air", {
   init: function () {
-    let grandmas = Game.Objects["Grandma"];
-    if (!grandmas) return false;
-    grandmas.art.bg = this.dir + "/backgroundWithWindow.png";
+    this.grandmas = Game.Objects["Grandma"];
+    if (!this.grandmas) return false;
+    const MOD = this;
+
+    Game.registerHook("check", function () {
+      MOD.updateGrandmaBackground();
+    });
+  },
+  updateGrandmaBackground: function () {
+    if (Game.elderWrath > 0) {
+      this.grandmas.art.bg = this.dir + "/backgroundWithFallingCookies.png";
+    } else {
+      this.grandmas.art.bg = this.dir + "/backgroundWithWindow.png";
+    }
   },
 });
